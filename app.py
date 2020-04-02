21075103e0c0b9ae7ee14178690f5a8dafd9d32a
yeshwenth <yeshwenth@users.noreply.github.com>
2020-04-01 23:33:09 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,16 +1,8 @@
-import numpy as np
-import pandas as pd
-import os
-import cv2
+from flask import Flask
 import keras
-import tensorflow as tf
-from flask import jsonify
-
-import base64
-
+from tensorflow import tp
 app = Flask(__name__)
 
- 
 @app.route('/')
 def hello_world():
-    return 'Hello, Ramji!'
+    return 'Hello, World!'
