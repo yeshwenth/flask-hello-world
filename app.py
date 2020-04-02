d3fb1b9f80a7d1b01d06d746dd35217efaab1fae
yeshwenth <yeshwenth@users.noreply.github.com>
2020-04-01 23:17:29 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,3 @@
-import dash
-import dash_core_components as dcc
-import dash_html_components as html
-from dash.dependencies import Input, Output, ClientsideFunction,State
-
 import numpy as np
 import pandas as pd
 import os
@@ -15,10 +10,7 @@ import base64
 
 app = Flask(__name__)
 
+ 
 @app.route('/')
 def hello_world():
-    return_dict = {
-                    'keras_api_version' :keras.__version__,
-    }
-    print(return_dict)
-    return jsonify(return_dict)
+    return 'Hello, Ramji!'
